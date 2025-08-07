e3b213aed330ebcde70a59bc9ab643eecb5f0bdd
Guilherme Corá <guilhermearcora@gmail.com>
2025-08-07 11:03:39 -0300

    projeto um

---
+++ expanding-cards/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//node list, similar a um array
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActive();
+        panel.classList.add('active')
+    });
+});
+
+const removeActive = function(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    });
+};
